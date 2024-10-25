b164a775de34b7657b9ce0d392201cc30928bac5
Daddah-Veronica-Afua <daddahveronica35@gmail.com>
2024-10-25 15:15:36 +0000

    Python

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+my_script = "my very eyes may just see under nine planets"
+print(my_script.upper())
+
+my_caps = "MY VERY EYES MAY JUST SEE UNDER NINE PLANETS"
+print(my_caps.lower())
+
+print(my_caps.replace('EYES','SIGHT'))
+
+print(my_caps.join("The nine Planets"))
+
